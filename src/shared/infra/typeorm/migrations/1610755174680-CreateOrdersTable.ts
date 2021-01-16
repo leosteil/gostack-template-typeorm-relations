53f40c50d847b5e3dd38d65a32c9a4b0a3041aeb
leosteil <lsteil@inf.ufsm.br>
2021-01-15 21:46:00 -0300

    Creating migrations

---
+++ src/shared/infra/typeorm/migrations/1610755174680-CreateOrdersTable.ts
@@ -0,0 +1,29 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateOrdersTable1610755174680
+    implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'orders',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
+                    },
+                    {
+                        name: 'quantity',
+                        type: 'int',
+                    },
+                ],
+            }),
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('orders');
+    }
+}
